0205bb0cc417f1476bf1592e9145a8bacb0186f4
Neil Wheatley <neilwheatley1980@gmail.com>
2020-06-04 09:12:16 +0100

    Test build with MMistakes theme. Missing pages e.g. categories

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "What is Dbljump?",
+        "excerpt":"Hello world, Dbljump is a video game knowledge reference.   You’re looking at a test deployment of our blog site.   Please check back soon!  ","categories": ["Mission"],
+        "tags": [],
+        "url": "https://blog.dbljump.com/pages/dbljump/blog/mission/what-is-dbljump/",
+        "teaser": null
+      }]
