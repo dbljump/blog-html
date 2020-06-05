6b71998600db2aad1ff9dc9e43c14a6bd7504f62
Neil Wheatley <neilwheatley1980@gmail.com>
2020-06-05 20:39:30 +0100

    Add categories pages, custom 404, minor improvements

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,6 +2,6 @@ var store = [{
         "title": "What is Dbljump?",
         "excerpt":"Hello world, Dbljump is a video game knowledge reference.   You’re looking at a test deployment of our blog site.   Please check back soon!  ","categories": ["Mission"],
         "tags": [],
-        "url": "https://blog.dbljump.com/mission/what-is-dbljump/",
-        "teaser": null
+        "url": "https://blog.dbljump.com/2020/06/03/what-is-dbljump/",
+        "teaser": "https://blog.dbljump.com/assets/images/default-post-image.jpg"
       }]
