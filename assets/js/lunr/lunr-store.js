7437136dbb2ede3050dd88a8f2559a3771dcc644
Neil Wheatley <neilwheatley1980@gmail.com>
2020-06-16 10:47:49 +0100

    Publishing first real post

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,7 +1,7 @@
 var store = [{
         "title": "What is Dbljump?",
-        "excerpt":"Hello world, Dbljump is a video game knowledge reference.   You’re looking at a test deployment of our blog site.   Please check back soon!  ","categories": ["Mission"],
+        "excerpt":"Summary: Dbljump is a mission to build the world’s best video game knowledge reference. It has the structure of a database, detailed prose like an encyclopaedia, and is 100% gaming focused. We’re launching an “alpha” version, with the goal of building the community and shaping development plans. Presentation and features...","categories": ["Mission"],
         "tags": [],
-        "url": "https://blog.dbljump.com/2020/06/03/what-is-dbljump/",
-        "teaser": "https://blog.dbljump.com/assets/images/default-post-image.jpg"
+        "url": "https://blog.dbljump.com/2020/06/15/what-is-dbljump/",
+        "teaser": "https://blog.dbljump.com/assets/images/2020/06/game-article.jpg"
       }]
