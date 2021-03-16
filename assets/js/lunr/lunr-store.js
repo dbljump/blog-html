b1f0734aa3a5962f33d1f7fccb75ac5c6a638b57
Neil Wheatley <neilwheatley1980@gmail.com>
2021-03-16 10:56:01 +0000

    Add post announcing signups

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,6 +2,12 @@ var store = [{
         "title": "What is Dbljump?",
         "excerpt":"Summary: Dbljump is a mission to build the world’s best video game knowledge reference. It has the structure of a database, detailed prose like an encyclopaedia, and is 100% gaming focused. We’re launching an “alpha” version, with the goal of building the community and shaping development plans. Presentation and features...","categories": ["Mission"],
         "tags": [],
-        "url": "https://blog.dbljump.com/2020/06/15/what-is-dbljump/",
-        "teaser": "https://blog.dbljump.com/assets/images/2020/06/game-article.jpg"
+        "url": "/2020/06/15/what-is-dbljump/",
+        "teaser": "/assets/images/2020/06/game-article.jpg"
+      },{
+        "title": "Dbljump editor signups are open -- here's what you need to know",
+        "excerpt":"Summary: You can now sign up to become a Dbljump editor! You’ll need to be approved before you can start editing. The project is evolving, and some features aren’t here yet, but as an editor you can help shape them. We plan to improve support materials for editors in the...","categories": ["Community"],
+        "tags": [],
+        "url": "/2021/03/16/dbljump-editor-signups-are-open/",
+        "teaser": "/assets/images/2021/03/dbljump-editor-community.jpg"
       }]
